02d1ca3111678abb31eaf94651dde617b1278d45
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-03-28 03:03:02 +0800

    集成native-UI，集成types/node等库、代码以解决nuxt.config.ts文件的问题

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
+<!--
+ * @Descripttion: 
+ * @Author: lwp
+ * @Date: 2023-03-28 00:42:47
+ * @LastEditTime: 2023-03-28 02:51:00
+-->
 <template>
   <div>
-    <NuxtWelcome />
+    <n-button type="primary" size="medium">按钮</n-button>
   </div>
 </template>
+
+<script setup>
+import {
+  NButton
+} from "naive-ui"
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,30 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+/*
+ * @Descripttion: 
+ * @Author: lwp
+ * @Date: 2023-03-28 00:42:47
+ * @LastEditTime: 2023-03-28 02:24:41
+ */
+import { defineNuxtConfig } from "nuxt/config";
 
-})
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  build: {
+    transpile:
+      process.env.NODE_ENV === "production"
+        ? [
+            "naive-ui",
+            "vueuc",
+            "@css-render/vue3-ssr",
+            "@juggle/resize-observer",
+          ]
+        : ["@juggle/resize-observer"],
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === "development"
+          ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
+          : [],
+    },
+  },
+});
