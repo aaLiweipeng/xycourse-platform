03e3fef2701e6a4f06f08e4daff73e1ba4062069
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-04 04:30:11 +0800

    跳转和动态路由页面设计

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
  * @Descripttion: 导航栏
  * @Author: lwp
  * @Date: 2023-04-02 17:04:01
- * @LastEditTime: 2023-04-03 01:38:22
+ * @LastEditTime: 2023-04-04 04:41:14
 -->
 <template>
   <div class="navbar">
@@ -10,7 +10,12 @@
       <!-- text属性 无边框，文本按钮 -->
       <n-button text class="!text-xl !font-bold">呵呵编程</n-button>
 
-      <div>导航区域</div>
+      <div class="menu">
+        <div class="menu-item" v-for="(item, index) in menus" :key="index"
+          :class="{ 'menu-item-active': route.path == item.path }" @click="handleOpen(item.path)">
+          {{ item.name }}
+        </div>
+      </div>
 
       <!-- 这里的marginLeft-auto 用的妙 -->
       <n-button circle class="ml-auto mr-3">
@@ -25,11 +30,7 @@
       <!-- https://www.naiveui.com/zh-CN/os-theme/components/dropdown -->
       <n-dropdown :options="userOptions">
         <!-- 下拉菜单外露图标 -->
-        <n-avatar
-          round
-          size="small"
-          src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
-        />
+        <n-avatar round size="small" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
       </n-dropdown>
     </div>
   </div>
@@ -53,7 +54,49 @@ const renderIcon = (icon) => {
   };
 };
 
-// 下拉菜单选项
+const route = useRoute();
+const menus = [
+  {
+    name: "首页",
+    path: "/",
+  },
+  {
+    name: "考试",
+    path: "/paper/1", // url/分页
+  },
+  {
+    name: "拼团",
+    path: "/list/group/1", // url/公共列表类型/分页
+  },
+  {
+    name: "秒杀",
+    path: "/list/flashsale/1",
+  },
+  {
+    name: "直播",
+    path: "/list/live/1",
+  },
+  {
+    name: "专栏",
+    path: "/list/column/1",
+  },
+  {
+    name: "电子书",
+    path: "/list/book/1",
+  },
+  {
+    name: "社区",
+    path: "/bbs/0/1", // url/社区id/分页
+  },
+  {
+    name: "课程",
+    path: "/list/course/1",
+  },
+];
+function handleOpen(path) {
+  navigateTo(path);
+}
+// NavBar右侧icon 下拉菜单选项
 const userOptions = [
   {
     label: "用户中心",
@@ -72,5 +115,20 @@ const userOptions = [
 .navbar {
   z-index: 1000;
   @apply bg-white fixed top-0 left-0 right-0 shadow-sm;
+}
+
+.menu {
+  @apply flex items-center px-4;
+}
+
+.menu-item {
+  transition: all 0.2s;
+  /* 让hover的显隐 有一个隐约的过程 */
+  /* 圆角 蓝底蓝字的hover */
+  @apply px-2 py-1 mx-1 cursor-pointer rounded hover: (bg-blue-50 text-blue-700) active:( !bg-blue-100);
+}
+
+.menu-item-active {
+  @apply !bg-blue-100 !text-blue-600;
 }
 </style>
