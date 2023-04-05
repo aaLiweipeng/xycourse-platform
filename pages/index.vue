84a99dc536170421cfedb848f4328d83a179cd59
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-06 02:40:26 +0800

    首页数据接口接入处理

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,25 @@
  * @Descripttion: 
  * @Author: lwp
  * @Date: 2023-04-02 00:14:09
- * @LastEditTime: 2023-04-04 04:12:54
+ * @LastEditTime: 2023-04-05 23:28:05
 -->
 <template>
     <div>
-        index
+        {{ data }}
     </div>
 </template>
 <script setup>
-
+const { 
+    data
+} = await useFetch("/index",{
+    key:"IndexData",
+    baseURL:"http://demonuxtapi.dishait.cn/pc",
+    headers:{
+        appid:"bd9d01ecc75dbbaaefce"
+    },
+    // 响应之前数据处理
+    transform:(res)=>{
+        return res.data
+    }
+})
 </script>
