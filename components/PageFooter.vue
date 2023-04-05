81ea904e6260391dbc0a1007cf657d7d0ea0e251
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-05 20:51:36 +0800

    底部组件

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,3 +1,17 @@
+<!--
+ * @Descripttion: 
+ * @Author: lwp
+ * @Date: 2023-04-02 17:04:01
+ * @LastEditTime: 2023-04-05 20:48:34
+-->
 <template>
-    底部
+  <div class="mt-auto bg-dark-400 text-gray-100">
+    <div class="flex items-center justify-center py-5">
+      <a href="#" v-for="i in 5" class="p-3">友情链接</a>
+    </div>
+    <!-- border-t border-gray-700  灰色上边框 -->
+    <div class="container border-t border-gray-700 flex items-center justify-center py-5">
+      <small> Copyright © 2022 dishait.cn 粤ICP备2020100536号 </small>
+    </div>
+  </div>
 </template>
