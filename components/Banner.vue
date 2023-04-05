c3e7c39e927875d0a9bdbd2e15e0d2daf75273ff
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-06 04:46:31 +0800

    完成轮播图

---
+++ components/Banner.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Descripttion: 
+ * @Author: lwp
+ * @Date: 2023-04-06 03:38:50
+ * @LastEditTime: 2023-04-06 03:44:18
+-->
+<template>
+  <n-carousel show-arrow class="mb-6">
+    <img
+      v-for="(item, index) in data"
+      :key="index"
+      class="carousel-img rounded cursor-pointer"
+      :src="item.src"
+      @click="open(item)"
+    />
+  </n-carousel>
+</template>
+
+<script setup>
+import { NCarousel } from 'naive-ui'
+
+defineProps({
+  data: Array
+})
+
+const open = (item) => {
+  if (item.type == 'webview') {
+    window.open(item.url)
+  }
+}
+</script>
+
+<style scoped>
+.carousel-img {
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,47 +2,39 @@
  * @Descripttion: 
  * @Author: lwp
  * @Date: 2023-04-02 00:14:09
- * @LastEditTime: 2023-04-06 03:14:14
+ * @LastEditTime: 2023-04-06 04:35:50
 -->
 <template>
-    <div>
-        <template v-if="pending">
-            加载中...
-        </template>
-        <template v-else-if="error">
-           错误提示： {{ error?.data?.data }}
-        </template>
-        <template v-else>
-            {{ data }}
-        </template>
-        
-    </div>
+  <div>
+    <template v-if="pending"> 加载中... </template>
+    <template v-else-if="error"> 错误提示： {{ error?.data?.data }} </template>
+    <template v-else>
+      <!-- 第一层data下有很多种类型的数据，遍历【v-for】到 swiper类型那个data，才给渲染【v-if】 -->
+      <template v-for="(item, index) in data" :key="index">
+        <Banner :data="item.data" v-if="item.type == 'swiper'" />
+      </template>
+    </template>
+  </div>
 </template>
 <script setup>
-const { 
-    pending,
-    data,
-    refresh,
-    error
-} = await useFetch("/index",{
-    key:"IndexData",
-    baseURL:"http://demonuxtapi.dishait.cn/pc",
-    headers:{
-        appid:"bd9d01ecc75dbbaaefce"
-    },
-    // 响应之前数据处理
-    transform:(res)=>{
-        return res.data
-    },
-    // 是否开启缓存
-    // initialCache:false,
-    // 懒加载
-    lazy:true
+const { pending, data, refresh, error } = await useFetch('/index', {
+  key: 'IndexData',
+  baseURL: 'http://demonuxtapi.dishait.cn/pc',
+  headers: {
+    appid: 'bd9d01ecc75dbbaaefce'
+  },
+  // 响应之前数据处理
+  transform: (res) => {
+    return res.data
+  },
+  // 是否开启缓存
+  // initialCache:false,
+  // 懒加载
+  lazy: true
 })
 
 // 服务端时直接报错
 // if(process.server && error.value){
 //     throwError(error.value?.data?.data)
 // }
-
 </script>
