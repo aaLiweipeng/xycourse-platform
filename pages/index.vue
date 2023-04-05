f2d0f49da34779be95afa6154f8c3ec6bc293603
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-06 03:20:49 +0800

    懒加载、error处理

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,28 @@
  * @Descripttion: 
  * @Author: lwp
  * @Date: 2023-04-02 00:14:09
- * @LastEditTime: 2023-04-05 23:28:05
+ * @LastEditTime: 2023-04-06 03:14:14
 -->
 <template>
     <div>
-        {{ data }}
+        <template v-if="pending">
+            加载中...
+        </template>
+        <template v-else-if="error">
+           错误提示： {{ error?.data?.data }}
+        </template>
+        <template v-else>
+            {{ data }}
+        </template>
+        
     </div>
 </template>
 <script setup>
 const { 
-    data
+    pending,
+    data,
+    refresh,
+    error
 } = await useFetch("/index",{
     key:"IndexData",
     baseURL:"http://demonuxtapi.dishait.cn/pc",
@@ -21,6 +33,16 @@ const {
     // 响应之前数据处理
     transform:(res)=>{
         return res.data
-    }
+    },
+    // 是否开启缓存
+    // initialCache:false,
+    // 懒加载
+    lazy:true
 })
+
+// 服务端时直接报错
+// if(process.server && error.value){
+//     throwError(error.value?.data?.data)
+// }
+
 </script>
