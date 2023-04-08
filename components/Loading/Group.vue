96e4fbca33f1cdb7807abc3ef67badca07a570a8
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-09 04:05:21 +0800

    新增骨架屏组件 优化加载效果

--- components/Loading/Group.vue
+++ components/Loading/Group.vue
@@ -2,11 +2,13 @@
  * @Descripttion: 全局页面加载组件封装
  * @Author: lwp
  * @Date: 2023-04-09 02:25:38
- * @LastEditTime: 2023-04-09 02:30:30
+ * @LastEditTime: 2023-04-09 04:02:27
 -->
 <template>
   <div>
-    <template v-if="pending"> 加载中... </template>
+    <template v-if="loading">
+      <LoadingSkeleton />
+    </template>
 
     <template v-else-if="error">
       <n-result class="mt-10" status="500" title="错误提示" :description="error?.data?.data || '页面走丢了~'">
@@ -24,8 +26,9 @@
 </template>
 
 <script setup>
+import { onBeforeUnmount } from 'vue'
 import { NButton, NResult } from 'naive-ui'
-defineProps({
+const props = defineProps({
   pending: {
     type: Boolean,
     default: false
@@ -35,4 +38,19 @@ defineProps({
     default: false
   }
 })
+
+const loading = ref(false) // loading骨架加载组件的开关
+const stop = watchEffect(() => {
+  if (props.pending && !loading.value) { // 如果页面加载中（pending）且 loading为空
+    loading.value = true
+  } else {
+    setTimeout(() => {
+      // watchEffect第一次调用时 如上，
+      // 再次调用时，必是stop被调用 或者loading为false 或者pending为false（任务加载完成）,
+      // 以上这三种情况，都要置loading为false
+      loading.value = false
+    }, 200)
+  }
+})
+onBeforeUnmount(() => stop())
 </script>
---
+++ components/Loading/Skeleton.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Descripttion: 加载中骨架屏封装
+ * @Author: lwp
+ * @Date: 2023-04-09 02:52:54
+ * @LastEditTime: 2023-04-09 02:53:22
+-->
+<template>
+  <n-card v-for="i in 3" :key="i" class="mb-5">
+    <n-skeleton text style="width: 30%" />
+    <n-skeleton text :repeat="3" />
+    <n-skeleton text style="width: 60%" />
+  </n-card>
+</template>
+<script setup>
+import { NCard, NSkeleton } from 'naive-ui'
+</script>
