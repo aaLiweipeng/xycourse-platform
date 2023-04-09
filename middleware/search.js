f24b6350f91b44f5a20c5b2fb14b8e7be5804271
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2023-04-09 22:52:31 +0800

    添加搜索结果页中间件验证

---
+++ middleware/search.js
@@ -0,0 +1,22 @@
+/*
+ * @Descripttion: 定义搜索功能的路由中间件
+ * @Author: lwp
+ * @Date: 2023-04-09 19:47:18
+ * @LastEditTime: 2023-04-09 22:20:28
+ */
+export default defineNuxtRouteMiddleware((to, from) => {
+  const { type, page } = to.params // url参数【同navigateTo函数位】
+  const { keyword } = to.query // query参数
+
+  console.log('6666666')
+
+  // 关键词判空
+  if (!keyword || (keyword && keyword.trim() == 0)) {
+    return abortNavigation('搜索关键词不能为空')
+  }
+
+  // 结果页url、分页参数的 合法性校验
+  if (!['course', 'column'].includes(type) || isNaN(+page)) {
+    return abortNavigation('页面不存在')
+  }
+})
--- pages/search/[type]/[page].vue
+++ pages/search/[type]/[page].vue
@@ -1,5 +1,15 @@
+<!--
+ * @Descripttion:
+ * @Author: lwp
+ * @Date: 2023-04-09 17:51:15
+ * @LastEditTime: 2023-04-09 21:14:43
+-->
 <template>
-    <div>
-        {{ $route.params.type }} ~ {{ $route.params.page }}
-    </div>
+  <div>{{ $route.params.type }} ~ {{ $route.params.page }}</div>
 </template>
+
+<script setup>
+definePageMeta({
+  middleware: ['search']
+})
+</script>
